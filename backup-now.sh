fb719bb688e3e84cdbb847e19903d6333e6e190d
Andrejus <anba8005@gmail.com>
2022-12-24 15:23:05 +0200

    simplified

--- backup-now.sh
+++ backup-now.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
-SNAPSHOT_NAME=$(curl $SNAPSHOT_CREATE_URL | jq -r '.snapshot')
-sleep 5
-
-REPLICA_COUNT=$(hostname | grep -Eo '[0-9]+$')
-eval "HEARTBEAT_URL=\$HEARTBEAT_CALLBACK_${REPLICA_COUNT}"
-eval "DST_URL=\$CUSTOM_S3_BASEPATH/$HOSTNAME/$SNAPSHOT_NAME"
-
 echo $SNAPSHOT_CREATE_URL
-echo $SNAPSHOT_NAME
 echo $CUSTOM_S3_ENDPOINT
 echo $CUSTOM_S3_BASEPATH
-echo $HOSTNAME
-echo $HEARTBEAT_URL
-echo $DST_URL
 
 sleeptime=10m # Sleep for 10 minutes after a failed try.
 maxtries=5    # 5 * 10 minutes = about 50 minutes total of waiting,
               # not counting running and failing.
 
-while ! /vmbackup-prod -storageDataPath=/storage -credsFilePath=/creds -snapshotName=$SNAPSHOT_NAME -customS3Endpoint=$CUSTOM_S3_ENDPOINT -dst=$DST_URL; do
+while ! /vmbackup-prod -storageDataPath=/victoria-metrics-data -snapshot.createURL=$SNAPSHOT_CREATE_URL -customS3Endpoint=$CUSTOM_S3_ENDPOINT -dst=$CUSTOM_S3_BASEPATH; do
   maxtries=$(( maxtries - 1 ))
   if [ "$maxtries" -eq 0 ]; then
     echo "Victoria metrics backup didn't succeed! Exiting." >&2
@@ -29,10 +18,3 @@ while ! /vmbackup-prod -storageDataPath=/storage -credsFilePath=/creds -snapshot
   sleep "$sleeptime" || break
 done
 
-STATUS=$(curl http://localhost:8482/snapshot/delete?snapshot=$SNAPSHOT_NAME | jq -r '.status')
-
-if [ $STATUS == "ok" ]; then
-  if [ -n "$HEARTBEAT_URL" ]; then
-    curl $HEARTBEAT_URL
-  fi
-fi
